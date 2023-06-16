9664cbecb87bd9d277cd61915fae0474e0b509ce
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-06-15 21:25:16 -0700

    committed everything

--- index.php
+++ index.php
@@ -31,8 +31,7 @@ get_header(); ?>
 						get_template_part( 'template-parts/content', get_post_type() );
 
 					endwhile;
-
-					the_posts_navigation();
+					the_posts_pagination();
 
 				else :
 
