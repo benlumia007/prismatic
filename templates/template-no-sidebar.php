1aed3c8aba3f4b09599f9b753380a68c8847ee5f
Benjamin Lu <benlumia007@users.noreply.github.com>
2023-06-27 15:26:06 -0700

    updated

--- templates/template-no-sidebar.php
+++ templates/template-no-sidebar.php
@@ -2,3 +2,30 @@
 /**
  * Template Name: No Sidebar
  */
+
+get_header(); ?>
+	<section class="site-content">
+		<div id="global-layouts" class="no-sidebar">
+			<main id="primary" class="site-main">
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the Post-Type-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', 'page' );
+
+				endwhile;
+				comments_template();
+				?>
+
+			</main><!-- #main -->
+			<?php get_sidebar('secondary'); ?>
+		</div>
+	</section>
+
+<?php
+get_footer();
